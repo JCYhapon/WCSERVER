021649920550abc68a217ef3f17e313830e1a8a9
JCYhapon <98503854+JCYhapon@users.noreply.github.com>
2022-03-02 13:13:36 +0800

    Add files via upload

---
+++ express-post/server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+
+
+app.use(express.static('public'))
+app.get('/index.html', function(req, res){
+    res.sendFile(__dirname + "/" + "index.html");
+})
+
+app.post('/process_post', urlencodedParser, function(req, res){
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        course: req.body.course,
+        age: req.body.age,
+        comments: req.body.comments
+    };
+    console.log(response);
+    res.end(JSON.stringify(response))
+})  
+
+
+var server = app.listen (7000, function () {
+    var host = server.address().address; 
+    var port = server.address().port;
+
+    console.log("Server listening at http://%s:%s", host, port);
+  })
